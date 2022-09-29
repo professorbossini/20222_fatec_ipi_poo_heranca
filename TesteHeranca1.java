664c658212a0cb175cf582191948ac71a817c706
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-29 15:58:00 -0300

    heranca(apostila05): fim da listagem 1.3.2.2

---
+++ TesteHeranca1.java
@@ -0,0 +1,14 @@
+public class TesteHeranca1 {
+  public static void main(String[] args) {
+    //vejamos se alunos de graduação têm nome e idade
+    //construtor que não recebe coisa alguma chama: construtor padrão 
+    AlunoDeGraduacao aluno = new AlunoDeGraduacao();
+    aluno.nome = "João";
+    aluno.idade = 17;
+    System.out.printf(
+      "Nome: %s, idade: %d\n",
+      aluno.nome,
+      aluno.idade
+    );
+  }
+}
